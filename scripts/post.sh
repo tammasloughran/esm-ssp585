1c34a6eef6bbe57dd80b0bb59bc26aa232b4bd02
Scott Wales <scott.wales@unimelb.edu.au>
2021-04-29 11:01:20 +1000

    2021-04-29 11:01:20: Run 3

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 set -eu
 
-# Update land use field to be correct for the current date in the final restart file output by this run
-
-last_restart=$(ls work/atmosphere/aiihca.d* | tail -n 1)
-./scripts/update_landuse.py $last_restart
--- scripts/pre.sh
+++ scripts/pre.sh
@@ -5,4 +5,8 @@ pwd
 
 # Update land use field to be correct for the current date in the current restart file
 
-./scripts/update_landuse.py work/atmosphere/restart_dump.astart
+year=$(mule-pumf --component fixed_length_header work/atmosphere/restart_dump.astart | sed -n 's/.*t2_year\s*:\s*//p')
+
+cdo selyear,$(( year + 1 )) -chname,fraction,field1391 work/atmosphere/INPUT/cableCMIP6_LC_1850-2015.nc work/atmosphere/land_frac.nc
+mv work/atmosphere/restart_dump.astart work/atmosphere/restart_dump.astart.orig
+python scripts/update_cable_vegfrac.py -i work/atmosphere/restart_dump.astart.orig -o work/atmosphere/restart_dump.astart -f work/atmosphere/land_frac.nc -v
