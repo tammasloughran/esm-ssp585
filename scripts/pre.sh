17f9b2e9733bd37c0a25e711e75833455e038309
Scott Wales <scott.wales@unimelb.edu.au>
2021-05-06 13:50:03 +1000

    2021-05-06 13:50:02: Run 1

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -13,7 +13,7 @@ lu_file=$1
 # Update land use field to be correct for the current date in the current restart file
 year=$(mule-pumf --component fixed_length_header work/atmosphere/restart_dump.astart | sed -n 's/.*t2_year\s*:\s*//p')
 
-if cdo selyear,$(( year + 1 )) -chname,fraction,field1391 $lu_file work/atmosphere/land_frac.nc; then
+if cdo selyear,$(( year )) -chname,fraction,field1391 $lu_file work/atmosphere/land_frac.nc; then
 
     mv work/atmosphere/restart_dump.astart work/atmosphere/restart_dump.astart.orig
     python scripts/update_cable_vegfrac.py -i work/atmosphere/restart_dump.astart.orig -o work/atmosphere/restart_dump.astart -f work/atmosphere/land_frac.nc
